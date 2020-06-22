bb9c918324b39b5f2016b07531189cd49e5dcd2e
David Hale <davidhale87@gmail.com>
2020-06-22 13:48:29 -0500

    Added start date and end to be 2020. And opens to a new tab.

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 
 // how many articles to return
 
-var inputVar = "election"
+var inputVar = "pro"
+var startDate = "2020"
+var endDate = "2020"
 var userRequest = 5;
 newYorkTimesSearch(inputVar);
 function newYorkTimesSearch(inputVar){
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+inputVar+"&api-key=QtRFNmXuQA2YIuCxuiGyKQKp9cWc1QY9";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+inputVar+"&facet_fields=source&facet=true&begin_date="+startDate+"0101&end_date="+endDate+"1231&api-key=QtRFNmXuQA2YIuCxuiGyKQKp9cWc1QY9";
 
 $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(intialPull) {
-    console.log(intialPull.response.docs[0].headline.main)
+   
     console.log(intialPull);
 
     // This is the number of articles to return.
@@ -20,11 +22,12 @@ $.ajax({
       var tRow = $('<div>')
         //var articleHeadline = $("<tr>").text(intialPull.response.docs[i].headline.main)
         var fullArticleLink = $("<a>").attr("href",intialPull.response.docs[i].web_url)
+        fullArticleLink.attr("target", "_blank");
         var articleAbstract = $("<tr>").text(intialPull.response.docs[i].abstract)
-       
+        var articleByline = $("<tr>").text(intialPull.response.docs[i].byline.original)
         fullArticleLink.text(intialPull.response.docs[i].headline.main)
-        tRow.append(articleAbstract, fullArticleLink)
-        $('body').append(tRow)
+        tRow.append(fullArticleLink,articleAbstract, articleByline)
+        $('body').append(tRow) 
         
       
       
